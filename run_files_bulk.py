64710fee808abfbdd6351749b0edfeb29be6ea04
Kevin Mueller <kevinm36@uw.edu>
2018-09-08 04:37:45 -0400

    working bulk processes

---
+++ run_files_bulk.py
@@ -0,0 +1,26 @@
+import numpy as np
+import subprocess
+
+from tqdm import tqdm
+mode = 0
+
+num_points = 4
+num_modes = 2
+max_current_processes = 2
+
+assert num_points%num_modes == 0
+
+
+for m in range(num_modes):
+    for p in tqdm(range(num_points//max_current_processes)):
+        for i in range(max_current_processes):
+
+            if i == max_current_processes-1:
+                subprocess.Popen(["python vae_mnist.py {} {}".format(i,m)], shell=True).wait()
+            else:
+                proc = subprocess.Popen(["python vae_mnist.py {} {}".format(i,m)], shell=True)#,
+
+
+                 #stdin=None, stdout=None, stderr=None, close_fds=True)
+
+
